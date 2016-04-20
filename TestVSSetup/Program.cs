69b3707f6c9dc200806a6067b1433e00450303a5
Hover Zuo <hzuo@steadyrain.com>
2016-04-20 18:54:07 +0800

    Init VS Setup test;

---
+++ TestVSSetup/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using WinBaton.Control;
+using WinBaton.Service;
+
+namespace TestVSSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = Process.Start(@"H:\vs_enterprise.exe");
+
+            dynamic win = new Window(proc);
+            
+            //Button btnUpdate = win[AutomationId: "UpdateButton"];
+            Button btnCancel = win[AutomationId: "CancelButton"];
+
+            Sync.TryWaitFor(() =>
+            {
+                var vsProc = Process.GetProcessesByName("vs_enterprise");
+                win = new Window(vsProc.First());
+                btnCancel = win[AutomationId: "CancelButton"];
+                return btnCancel != null;
+            }, timeout: 1000 * 60 * 60 * 2, interval: 1000 * 60);
+
+            btnCancel.Click();
+
+            //btn.Click();
+        }
+    }
+}
